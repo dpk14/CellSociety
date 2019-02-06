3d66f273fd43352996f6b3cfd49bcf377e1fedb5
Jorge Raad <jar121@duke.edu>
2019-02-06 02:11:44 -0500

    started implementing Grid instead of Cell[][]

--- src/mainpackage/Grid.java
+++ src/mainpackage/Grid.java
@@ -1,11 +1,17 @@
 package mainpackage;
 
 import cells.Cell;
+import cells.StateChangeCell;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Grid {
+    private Cell[][] myCellArray;
+
+    public Grid(int rows, int columns, List<Cell> list){
+        myCellArray = getNewGrid(rows, columns, list);
+    }
 
     protected List<Cell> getImmediateNeighbors(Cell cell, Cell[][] myGrid){
       return new ArrayList<Cell>();
@@ -14,5 +20,36 @@ public abstract class Grid {
     protected List<Cell> getAllNeighbors(Cell cell, Cell[][] myGrid){
         return new ArrayList<Cell>();
     }
+
+    protected Cell[][] getNewGrid(int rows, int columns, List<Cell> list){
+        Cell[][] newGrid = new Cell[rows][columns];
+        for(Cell cell : list){
+            newGrid[cell.getRow()][cell.getColumn()] = cell;
+        }
+        return newGrid;
+    }
+
+
+    protected List<Cell> getTypedNeighbors(Cell cell, String type) {
+        List<Cell> neighbors = getNeighbors(cell);
+        List<Cell> specificNeighbors=new ArrayList<Cell>();
+        for(Cell neighbor: neighbors){
+            if (((StateChangeCell) neighbor).getState().equals(type)) specificNeighbors.add(neighbor);
+        }
+        return specificNeighbors;
+    }
+
+    protected Cell getCell(int row, int column){
+        return myCellArray[row][column];
+    }
+
+    protected int getHeight(){
+        return myCellArray.length;
+    }
+
+    protected int getWidth(){
+        return myCellArray[0].length;
+    }
+
 
 }
--- src/simulations/Simulation.java
+++ src/simulations/Simulation.java
@@ -2,13 +2,14 @@ package simulations;
 
 import cells.Cell;
 import cells.StateChangeCell;
+import mainpackage.Grid;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public abstract class Simulation {
-    protected Cell[][] myGrid;
+    protected Grid myGrid;
     protected List<Cell> myCellList = new ArrayList<Cell>();
 
     public static enum Bounds{
@@ -83,55 +84,11 @@ public abstract class Simulation {
      */
     public abstract Cell[][] updateGrid();
 
-    protected Cell[][] getNewGrid(List<Cell> list){
-        Cell[][] newGrid = myGrid;
-        for(Cell cell : list){
-            newGrid[cell.getRow()][cell.getColumn()] = cell;
-        }
-        return newGrid;
-    }
-
-    /**
-     * Returns an ArrayList of cells that neighbor the given cell. In this case, it is implemented taking neighbros only
-     * to mean the cells immediately above, below, or next to the given cell. Depending on what "neighbors" means, may
-     * be overridden. A key assumption is that each row in the grid has the same number of entries.
-     *
-     * @param cell - cell whose neighbors will be checked.
-     * @return ArrayList<Cell> neighbors
-     */
-    protected List<Cell> getNeighbors(Cell cell){
-        List <Cell> neighbors = new ArrayList<>();
-        int row = cell.getRow();
-        int column = cell.getColumn();
-        if(row != 0){ // check top neighbor
-            neighbors.add(myGrid[row-1][column]);
-        }
-        if(row != myGrid.length-1){ // check bottom neighbor
-            neighbors.add(myGrid[row+1][column]);
-        }
-        if(column != 0){ // check left neighbor
-            neighbors.add(myGrid[row][column-1]);
-        }
-        if(column != myGrid[0].length-1){ // check right neighbor
-            neighbors.add(myGrid[row][column+1]);
-        }
-        return neighbors;
-    }
-
-    protected List<Cell> getTypedNeighbors(Cell cell, String type) {
-        List<Cell> neighbors = getNeighbors(cell);
-        List<Cell> specificNeighbors=new ArrayList<Cell>();
-        for(Cell neighbor: neighbors){
-            if (((StateChangeCell) neighbor).getState().equals(type)) specificNeighbors.add(neighbor);
-        }
-        return specificNeighbors;
-    }
-
     /**
      * Returns grid at start of initialization just so I can check we have the right grid to begin with
      * @return
      */
-    public Cell[][] getMyGrid() {
+    public Grid getMyGrid() {
         return myGrid;
     }
 
